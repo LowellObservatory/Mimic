e0f6c11b49ea07fd0ab6314624baea7ffad8f4a3
Dyer M Lytle <dlytle@lowell.edu>
2020-06-25 09:40:12 -0700

    Update DomeControl.py

--- src/DomeControl.py
+++ src/DomeControl.py
@@ -3,19 +3,19 @@ import GPIOControl as ctl
  
 class DomeControl:
  
-  def __init__(self):
+  def __init__(self, domestate):
     # Initialize Dome Status
  
   def go_home (wait):
   def close_shutter (wait):
  
-  def open_shutter (Dome.State, wait):
-    ctl.upper_shutter_open(Dome.State, wait)
-    ctl.lower_shutter_open (Dome.State, wait)
+  def open_shutter (domestate, wait):
+    ctl.upper_shutter_open(domestate, wait)
+    ctl.lower_shutter_open (domestate, wait)
   
   def goto_azimuth (azimuth):
-    leftRight = calculate_direction(Dome.State)
-    timeToMove = calculate_move_time(Dome.State)
+    leftRight = calculate_direction(domestate)
+    timeToMove = calculate_move_time(domestate)
     if (timeToMove > 1.0)):          # seconds
       if (leftRight == 'left'):
         ctl.rotate_left(timeToMove)
