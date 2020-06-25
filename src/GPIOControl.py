c83f31f8be56816665c6062fb96afaf80706c01e
Dyer M Lytle <dlytle@lowell.edu>
2020-06-25 09:27:38 -0700

    Update GPIOControl.py

--- src/GPIOControl.py
+++ src/GPIOControl.py
@@ -3,7 +3,7 @@ import time
  
 class GPIOControl:
  
-  def __init__(self, DomeState):
+  def __init__(self, domestate):
     GPIO.setmode (GPIO.BOARD)
     chan_list = [5, 6, 16, 17, 22, 23, 24, 25, 26, 27]
     GPIO.setup (chan_list, GPIO.OUT)
@@ -13,37 +13,37 @@ class GPIOControl:
     GPIO.output ([26, 23, 24], (GPIO_HIGH, GPIO_LOW, GPIO_HIGH)
     time.sleep(wait)
     GPIO.output ([26, 23, 24], (GPIO_HIGH, GPIO_HIGH, GPIO_HIGH)
-    DomeState.Upper_Shutter = "open"
+    domestate.Upper_Shutter = "open"
  
   def upper_shutter_close (wait):
     GPIO.output ([26, 23, 24], (GPIO_LOW, GPIO_HIGH, GPIO_LOW)
     time.sleep(wait)
     GPIO.output ([26, 23, 24], (GPIO_HIGH, GPIO_HIGH, GPIO_HIGH)
-    DomeState.Upper_Shutter = "closed"
+    domestate.Upper_Shutter = "closed"
                  
   def lower_shutter_open (wait):
     GPIO.output ([6, 25, 16], (GPIO_HIGH, GPIO_LOW, GPIO_HIGH)
     time.sleep(wait)
     GPIO.output ([6, 25, 16], (GPIO_HIGH, GPIO_HIGH, GPIO_HIGH)
-    DomeState.Lower_Shutter = "open"
+    domestate.Lower_Shutter = "open"
                  
   def lower_shutter_close (wait):
     GPIO.output ([6, 25, 16], (GPIO_LOW, GPIO_HIGH, GPIO_LOW)
     time.sleep(wait)
     GPIO.output ([6, 25, 16], (GPIO_HIGH, GPIO_HIGH, GPIO_HIGH)
-    DomeState.Lower_Shutter = "closed"
+    domestate.Lower_Shutter = "closed"
                  
   def rotate_left (wait):
-    DomeState.Moving = "True"
+    domestate.isMoving = "True"
     GPIO.output ([27, 5, 17], (GPIO_HIGH, GPIO_LOW, GPIO_HIGH)
     time.sleep(wait)
     GPIO.output ([27, 5, 17], (GPIO_HIGH, GPIO_HIGH, GPIO_HIGH)
-    DomeState.Moving = "False"
+    domestate.isMoving = "False"
                  
   def rotate_right (wait):
-    DomeState.Moving = "True"
+    domestate.isMoving = "True"
     GPIO.output ([27, 5, 17], (GPIO_LOW, GPIO_HIGH, GPIO_LOW)
     time.sleep(wait)
     GPIO.output ([27, 5, 17], (GPIO_HIGH, GPIO_HIGH, GPIO_HIGH)
-    DomeState.Moving = "False"
+    domestate.isMoving = "False"
  
